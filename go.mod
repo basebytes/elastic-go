7360fa56e5b69cafc2b7997435a3c5ddf1593a62
runnerhbc <hbcjava@vip.qq.com>
2022-08-24 19:48:06 +0800

    update service bulk function

--- go.mod
+++ go.mod
@@ -3,12 +3,10 @@ module github.com/basebytes/elastic-go
 go 1.16
 
 require (
+	github.com/basebytes/elastic-go/client v0.0.3
+	github.com/basebytes/elastic-go/service v0.0.3
 	github.com/basebytes/tools v0.0.1
-	github.com/basebytes/elastic-go/client v0.0.2
-	github.com/basebytes/elastic-go/service v0.0.2
 	github.com/mitchellh/mapstructure v1.4.1
 )
 
-replace (
-    github.com/basebytes/elastic-go/service v0.0.2 => ./service
-)
+replace github.com/basebytes/elastic-go/service v0.0.3 => ./service
--- service/service.go
+++ service/service.go
@@ -13,199 +13,204 @@ import (
 	"strings"
 )
 
-type Service struct{
+type Service struct {
 	Constructor *constructor.Constructor
-	client *client.Client
+	client      *client.Client
 }
 
-func NewDefaultService() (service *Service,err error){
+func NewDefaultService() (service *Service, err error) {
 	var esClient *client.Client
-	if esClient,err=client.NewDefaultClient();err==nil{
-		service=&Service{
-			Constructor:constructor.New(),
-			client:esClient,
+	if esClient, err = client.NewDefaultClient(); err == nil {
+		service = &Service{
+			Constructor: constructor.New(),
+			client:      esClient,
 		}
 	}
-	return service,err
+	return service, err
 }
 
-func NewService(config *client.Config) (*Service,error){
-	service:=&Service{
-		Constructor:constructor.New(),
+func NewService(config *client.Config) (*Service, error) {
+	service := &Service{
+		Constructor: constructor.New(),
 	}
-	esClient,err:=client.NewClient(config)
-	if err!=nil{
+	esClient, err := client.NewClient(config)
+	if err != nil {
 		return nil, err
 	}
-	service.client=esClient
-	return service,nil
+	service.client = esClient
+	return service, nil
 }
 
-func (service *Service)Bulk(index string,docs []api.Doc) (*entity.DocumentBulkResult,error){
+func (service *Service) Bulk(index string, docs []api.Doc, failedInfoOnly bool) (*entity.DocumentBulkResult, error) {
 	var (
-		rsp *api.Response
-		err error
+		rsp    *api.Response
+		err    error
 		result *entity.DocumentBulkResult
-		bulk=service.client.Document.Bulk
+		bulk   = service.client.Document.Bulk
+		opt    []func(*api.DocumentBulkRequest)
 	)
-	if rsp,err=bulk(docs,bulk.WithIndex(index));err==nil{
-		result=new(entity.DocumentBulkResult)
-		err =rsp.Result(result)
+	opt = append(opt, bulk.WithIndex(index))
+	if failedInfoOnly {
+		opt = append(opt, bulk.WithFilterPath("items.*.error"))
 	}
-	return result,err
+	if rsp, err = bulk(docs, opt...); err == nil {
+		result = new(entity.DocumentBulkResult)
+		err = rsp.Result(result)
+	}
+	return result, err
 }
 
-func (service *Service)IndexExists(index string) (bool,error){
+func (service *Service) IndexExists(index string) (bool, error) {
 	var (
-		rsp *api.Response
-		err error
-		code =http.StatusInternalServerError
-		indexExists=service.client.Indexer.Exists
+		rsp         *api.Response
+		err         error
+		code        = http.StatusInternalServerError
+		indexExists = service.client.Indexer.Exists
 	)
-	if rsp,err=indexExists(indexExists.WithIndex(index));err==nil{
-		code=rsp.StatusCode
+	if rsp, err = indexExists(indexExists.WithIndex(index)); err == nil {
+		code = rsp.StatusCode
 	}
-	return code==http.StatusOK,err
+	return code == http.StatusOK, err
 }
 
-func (service *Service)GetIndexAlias(index, alias []string)(*entity.IndexAlias,error){
-	var(
-		rsp *api.Response
-		err error
-		result *entity.IndexAlias
-		aliasGet=service.client.Indexer.AliasGet
-	)
-	if rsp,err=aliasGet(aliasGet.WithIndex(index...),aliasGet.WithAlias(alias...));err==nil{
-		result=new(entity.IndexAlias)
-		err =rsp.Result(result)
-	}
-	return result,err
-}
-
-func (service *Service)GetIndexMapping(index string) (*entity.IndexMapping,error){
+func (service *Service) GetIndexAlias(index, alias []string) (*entity.IndexAlias, error) {
 	var (
-		rsp *api.Response
-		err error
-		result *entity.IndexMapping
-		getMapping=service.client.Indexer.GetMapping
+		rsp      *api.Response
+		err      error
+		result   *entity.IndexAlias
+		aliasGet = service.client.Indexer.AliasGet
 	)
-	if rsp,err=getMapping(getMapping.WithIndex(index));err==nil{
-		result=new(entity.IndexMapping)
-		err =rsp.Result(result)
+	if rsp, err = aliasGet(aliasGet.WithIndex(index...), aliasGet.WithAlias(alias...)); err == nil {
+		result = new(entity.IndexAlias)
+		err = rsp.Result(result)
 	}
-	return result,err
+	return result, err
 }
 
-func (service *Service)GetIndexSetting(index string,settings ...string)(*entity.IndexSetting,error){
-	var(
-		rsp *api.Response
-		err error
-		result *entity.IndexSetting
-		getSetting=service.client.Indexer.SettingsGet
-	)
-	if rsp,err=getSetting(getSetting.WithIndex(index),getSetting.WithSettings(settings...));err==nil{
-		result=new(entity.IndexSetting)
-		err=rsp.Result(result)
-	}
-	return result,err
-}
-
-func (service *Service)CreateIndex(index string,body *[]byte) (*entity.OperateResult,error){
+func (service *Service) GetIndexMapping(index string) (*entity.IndexMapping, error) {
 	var (
-		rsp *api.Response
-		err error
+		rsp        *api.Response
+		err        error
+		result     *entity.IndexMapping
+		getMapping = service.client.Indexer.GetMapping
+	)
+	if rsp, err = getMapping(getMapping.WithIndex(index)); err == nil {
+		result = new(entity.IndexMapping)
+		err = rsp.Result(result)
+	}
+	return result, err
+}
+
+func (service *Service) GetIndexSetting(index string, settings ...string) (*entity.IndexSetting, error) {
+	var (
+		rsp        *api.Response
+		err        error
+		result     *entity.IndexSetting
+		getSetting = service.client.Indexer.SettingsGet
+	)
+	if rsp, err = getSetting(getSetting.WithIndex(index), getSetting.WithSettings(settings...)); err == nil {
+		result = new(entity.IndexSetting)
+		err = rsp.Result(result)
+	}
+	return result, err
+}
+
+func (service *Service) CreateIndex(index string, body *[]byte) (*entity.OperateResult, error) {
+	var (
+		rsp    *api.Response
+		err    error
 		result *entity.OperateResult
-		create=service.client.Indexer.Create
+		create = service.client.Indexer.Create
 	)
-	if rsp,err=create(index,create.WithBody(body));err==nil{
-		result=new(entity.OperateResult)
-		err=rsp.Result(result)
+	if rsp, err = create(index, create.WithBody(body)); err == nil {
+		result = new(entity.OperateResult)
+		err = rsp.Result(result)
 	}
-	return result,err
+	return result, err
 }
 
-func (service *Service)DeleteIndex(index ...string) (*entity.OperateResult,error){
+func (service *Service) DeleteIndex(index ...string) (*entity.OperateResult, error) {
 	var (
 		rsp         *api.Response
 		err         error
 		result      *entity.OperateResult
-		indexDelete =service.client.Indexer.Delete
+		indexDelete = service.client.Indexer.Delete
 	)
-	if rsp,err= indexDelete(index);err==nil{
-		result=new(entity.OperateResult)
-		err=rsp.Result(result)
+	if rsp, err = indexDelete(index); err == nil {
+		result = new(entity.OperateResult)
+		err = rsp.Result(result)
 	}
-	return result,err
+	return result, err
 }
 
-func (service *Service)GetIndexNames(names ...string) ([]string,error){
+func (service *Service) GetIndexNames(names ...string) ([]string, error) {
 	var (
-		rsp         *api.Response
-		err         error
-		indices []string
-		catIndices =service.client.Cat.Indices
+		rsp        *api.Response
+		err        error
+		indices    []string
+		catIndices = service.client.Cat.Indices
 	)
-	if rsp,err= catIndices(catIndices.WithIndex(names...),catIndices.WithColumns("index"));err==nil{
+	if rsp, err = catIndices(catIndices.WithIndex(names...), catIndices.WithColumns("index")); err == nil {
 		indices = strings.Split(strings.TrimSpace(rsp.ResultString()), "\n")
 		sort.Strings(indices)
 	}
-	return indices,err
+	return indices, err
 }
 
-func(service *Service)UpdateIndexAlias(actions ...*api.AliasAction)(*entity.OperateResult,error){
+func (service *Service) UpdateIndexAlias(actions ...*api.AliasAction) (*entity.OperateResult, error) {
 	var (
-		rsp         *api.Response
-		err         error
-		result      *entity.OperateResult
-		aliases=service.client.Indexer.Aliases
+		rsp     *api.Response
+		err     error
+		result  *entity.OperateResult
+		aliases = service.client.Indexer.Aliases
 	)
 	fmt.Println(tools.Encode(actions))
-	if rsp,err=aliases(actions);err==nil{
-		result=new(entity.OperateResult)
-		err=rsp.Result(result)
+	if rsp, err = aliases(actions); err == nil {
+		result = new(entity.OperateResult)
+		err = rsp.Result(result)
 	}
-	return result,err
+	return result, err
 }
 
-func (service *Service)Ping() bool{
-	_,err:=service.client.Ping()
-	return err==nil
+func (service *Service) Ping() bool {
+	_, err := service.client.Ping()
+	return err == nil
 }
 
-func (service *Service)Search(indexName string,body *[]byte)(*entity.EsQueryResult,error){
-	var result      *entity.EsQueryResult
-	searcher:=service.client.Searcher.Search
-	resp,err:=searcher(
+func (service *Service) Search(indexName string, body *[]byte) (*entity.EsQueryResult, error) {
+	var result *entity.EsQueryResult
+	searcher := service.client.Searcher.Search
+	resp, err := searcher(
 		searcher.WithIndex(indexName),
 		searcher.WithBody(body),
 		searcher.WithTrackTotalHits(true),
-		)
-	if err==nil{
-		err=resp.Result(&result)
+	)
+	if err == nil {
+		err = resp.Result(&result)
 	}
 
-	return result,err
+	return result, err
 }
 
-func (service *Service)CatIndices(indices string)([]*entity.IndexInfo,error){
+func (service *Service) CatIndices(indices string) ([]*entity.IndexInfo, error) {
 	var (
-		result  []*entity.IndexInfo
-		esErr entity.EsError
+		result []*entity.IndexInfo
+		esErr  entity.EsError
 	)
-	cats:=service.client.Cat.Indices
-	resp,err:=cats(
+	cats := service.client.Cat.Indices
+	resp, err := cats(
 		cats.WithIndex(indices),
 		cats.WithFormat("json"),
-		)
-	if err==nil{
-		if resp.IsError(){
-			if err=resp.Result(&esErr);err==nil{
-				err=errors.New(esErr.Error)
+	)
+	if err == nil {
+		if resp.IsError() {
+			if err = resp.Result(&esErr); err == nil {
+				err = errors.New(esErr.Error)
 			}
-		}else{
-			err=resp.ResultJson(&result)
+		} else {
+			err = resp.ResultJson(&result)
 		}
 	}
 
-	return result,err
+	return result, err
 }
